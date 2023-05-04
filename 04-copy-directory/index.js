65d62f0fc23292b80d3ec987b706485c41749fdd
stroich <stroichmwork@gmail.com>
2023-05-04 18:05:03 +0400

    refactor: apply eslint

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,36 +1,36 @@
-function copyDir(){
-    const fs = require('fs');
-    const { mkdir } = require('node:fs/promises');
-    const { join } = require('node:path');
+const fs = require('fs');
+const { mkdir } = require('node:fs/promises');
+const { join } = require('node:path');
 
-    const projectFolderCopy = join(__dirname, 'files-copy');
-    const projectFolder = join(__dirname,'files');
+function copyDir() {
+  const projectFolderCopy = join(__dirname, 'files-copy');
+  const projectFolder = join(__dirname, 'files');
 
-    mkdir(projectFolderCopy, { recursive: true });
-    function deleteFiles(){
-        fs.readdir(projectFolderCopy,(error,filesСopy)=>{
-            if (error) throw error;
-            if(filesСopy.length > 0){
-                filesСopy.forEach(fileCope=>{
-                    const projectFileCopy = join( projectFolderCopy, fileCope);
-                    fs.unlink(projectFileCopy, error=>{
-                        if (error) throw error;
-                    })
-                })
-            }        
-        })
-    }
-    function copyFiles(){
-        fs.readdir(projectFolder,(error,files)=>{
-            if (error) throw error;
-            files.forEach(file=>{
-                const projectFile =join(projectFolder, file);
-                const projectFileCopy = join(projectFolderCopy, file);
-                fs.promises.copyFile(projectFile,projectFileCopy);
-            })
-        })
-    }
-    deleteFiles();
-    copyFiles();  
+  mkdir(projectFolderCopy, { recursive: true });
+  function deleteFiles() {
+    fs.readdir(projectFolderCopy, (error, filesСopy) => {
+      if (error) throw error;
+      if (filesСopy.length > 0) {
+        filesСopy.forEach((fileCope) => {
+          const projectFileCopy = join(projectFolderCopy, fileCope);
+          fs.unlink(projectFileCopy, (err) => {
+            if (err) throw err;
+          });
+        });
+      }
+    });
+  }
+  function copyFiles() {
+    fs.readdir(projectFolder, (error, files) => {
+      if (error) throw error;
+      files.forEach((file) => {
+        const projectFile = join(projectFolder, file);
+        const projectFileCopy = join(projectFolderCopy, file);
+        fs.promises.copyFile(projectFile, projectFileCopy);
+      });
+    });
+  }
+  deleteFiles();
+  copyFiles();
 }
 copyDir();
